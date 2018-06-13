8915e70251883e29584bb8c50dcdaacc0482a009
angelvillar96 <angelvillarcorrales@hotmail.com>
2018-06-13 21:46:12 +0200

    Started with the development of the API and the API docs

---
+++ api/stratosApiRest.py
@@ -0,0 +1,53 @@
+import os, time, sqlite3
+from datetime import datetime
+from flask import Flask, render_template, request, jsonfy
+
+
+app = Flask(__name__)
+
+
+#method that gets the URI parameters
+def getParams():
+	try:
+			os.system("clear")
+			params = request.form["params"]
+	except:
+			params = ""
+
+	return (params)
+
+
+#main web site
+@app.route('/')
+def initPage():
+    return(render_template('init.html'))
+
+
+#docs web site
+@app.route('/docs')
+def docsPage():
+    return(render_template('docs.html'))
+
+
+#parametes explanation web site
+@app.route('/params')
+def paramsPage():
+    return(render_template('params.html'))
+
+
+#exampels web site
+@app.route('/examples')
+def examplesPage():
+    return(render_template('examples.html'))
+
+
+#search method
+#@app.route('/examples')
+#def examplesPage():
+#    return(render_template('examples.html'))
+
+
+#main method
+if __name__ == '__main__':
+
+	app.run(debug = True, host = '0.0.0.0')
